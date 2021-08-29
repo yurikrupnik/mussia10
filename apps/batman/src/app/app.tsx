d5f988ea22ecb4d41d17759993606bb751b567ff
yurikrupnik <krupnik.yuri@gmail.com>
2021-08-29 22:24:18 +0300

    added 2 apps

--- apps/batman/src/app/app.tsx
+++ apps/batman/src/app/app.tsx
@@ -12,32 +12,6 @@ export function App() {
         <Logo width="75" height="75" />
         <h1>Welcome to batman!</h1>
       </header>
-      <main>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-  `}</pre>
-        </details>
-      </main>
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
