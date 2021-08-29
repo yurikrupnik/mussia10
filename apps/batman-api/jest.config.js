9bf671507d20f70c595be10c197cfd9c818e2b46
yurikrupnik <krupnik.yuri@gmail.com>
2021-08-29 22:06:45 +0300

    added 2 apps

---
+++ apps/batman-api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'batman-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/batman-api',
+};
