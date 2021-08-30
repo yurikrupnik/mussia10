5d71c508b2e84e1cfeda5b665f71e460c29be603
yurikrupnik <krupnik.yuri@gmail.com>
2021-08-30 03:36:52 +0300

    fixed code and tests passing

--- Makefile
+++ Makefile
@@ -2,4 +2,10 @@ run-one:
 	npx nx run batman:serve
 
 run-many:
-	npx nx run-many --target=serve --projects=batman,batman-api --parallel=true
+	npx nx run-many --target=${target} --projects=${projects} --parallel=true
+
+
+ra:
+	npx nx run-many --target=${target} --parallel --all
+raa:
+	npx nx affected --target=${target} --parallel --all
--- apps/batman-api/src/app/app.service.ts
+++ apps/batman-api/src/app/app.service.ts
@@ -1,26 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-const data: any = [
-  {
-    name: 'yuri',
-    age: 12,
-    height: 160
-  },
-  {
-    name: 'aris',
-    age: 13,
-    height: 160
-  },
-  {
-    name: 'musso',
-    age: 14,
-    height: 160
-  }
-]
-
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return data;
+    return { message: 'Welcome to batman-api!' };
   }
 }
